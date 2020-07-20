3db204684cb88ebcc89b8f0dd238b89afaddca1e
Jack Mordaunt <jackmordaunt@gmail.com>
2020-07-20 13:44:16 +0800

    feat: debug button.

--- dev.workflow
+++
@@ -1,8 +0,0 @@
-use code-insiders as editor;
-use http-server;
-use rollup;
-use watchexec;
-
-editor . 
-http-server .
-watchexec --clear --exts css,html,rs "wasm-pack build --target web && rollup ./main.js --format iife --file ./pkg/bundle.js" 
--- src/app.rs
+++ src/app.rs
@@ -1,24 +1,59 @@
+#![allow(unused)]
+
 use yew::prelude::*;
 
-pub struct App {}
+pub struct App {
+    pub link: ComponentLink<Self>,
+    pub debug: bool,
+}
 
-pub enum Msg {}
+pub enum Msg {
+    ToggleDebug,
+    Nope,
+}
 
 impl Component for App {
     type Message = Msg;
     type Properties = ();
 
-    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
-        App {}
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        App { link, debug: false }
     }
 
-    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::ToggleDebug => {
+                self.debug = !self.debug;
+            }
+            Msg::Nope => {}
+        };
         true
     }
 
     fn view(&self) -> Html {
+        let debug = if self.debug { "debug" } else { "" };
+        let toggle_debug = self.link.callback(|_| Msg::ToggleDebug);
+
         html! {
-            <p>{ "Hello Avisha!" }</p>
+            <grid class=debug>
+                <row>
+                    <nav>
+                        <brand>
+                            <h1 class="logo">
+                                {"Avisha"}
+                            </h1>
+                        </brand>
+                        <div>
+                            <button class="small" onclick=toggle_debug>
+                                {"Debug"}
+                            </button>
+                        </div>
+                    </nav>
+                </row>
+                <row>
+                    <col></col>
+                </row>
+            </grid>
         }
     }
 }
